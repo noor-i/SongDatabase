8abae301dff0c1f2dc569e9296113ef586159ccf
mia16 <mia16@sfu.ca>
2024-04-22 11:02:29 -0700

    added test cases for getter/setters

--- project.cpp
+++ project.cpp
@@ -1,10 +1,19 @@
 // main file
-#include <iostream>
 #include "Song.h"
-#include "Database.h"
-#include "Menu.h"
+//#include "Database.h"
+//#include "Menu.h"
 
 
 int main() {
+Song s1 = Song("indie", "K", "CAS", 2013);
+
+std::cout << "song artist: " << s1.getArtist()<< "\n";
+std::cout << "song genre: " << s1.getGenre()<< "\n";
+std::cout << "song title: " << s1.getTitle()<< "\n";
+std::cout << "song year: " << s1.getYear()<< "\n";
+
+s1.setArtist("Cigarettes After Sex");
+
+std::cout << "song artist: " << s1.getArtist()<< "\n";
 
 }
