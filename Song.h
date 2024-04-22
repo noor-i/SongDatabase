f4e469859222e05ed06a010d268b2a401d0db247
Noor <mahnur.imran@gmail.com>
2024-04-22 10:38:52 -0700

    added genre attribute to Song files

--- Song.h
+++ Song.h
@@ -5,27 +5,30 @@
 #include <string>
 
 class Song {
+    std::string genre;
     std::string title;
     std::string artist;
     int year;
 
 public:
     //Constructors
-    Song(){}
-    Song(std::string title, std::string artist, int year);
-    Song(const Song& other);
+    Song(){} // default constructor 
+    Song(std::string genre, std::string title, std::string artist, int year); // parameterized constructor 
+    Song(const Song& other); // copy constructor 
 
     //Destructor
     ~Song();
 
     //Getters
+    std::string getGenre() const;
     std::string getTitle() const;
     std::string getArtist() const;
     int getYear() const;
 
     //Setters
+    void setGenre(std::string genre);
     void setTitle(std::string title);
-    void setArtist(std::string title);
+    void setArtist(std::string artist);
     void setYear(int year);
 
 };
