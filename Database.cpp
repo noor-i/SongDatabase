8b3f66b193d28ba94d5f08953ed56128fc5c6365
Noor <mahnur.imran@gmail.com>
2024-04-22 23:09:29 -0700

    added number search methods for Database

--- Database.cpp
+++ Database.cpp
@@ -24,17 +24,16 @@ int Database::getCurrentSize() const{
     return numSongs;
 }
 
-// Add a record
 /*
 (1 mark) When the user chooses to enter a record, then your program asks
 them to enter each piece of data in the record. After the user enters valid
 data, then your program should say the record has been added, and go back to
 the main menu.
 
-******(1 mark) If the record already exists in the database, then you should
+(1 mark) If the record already exists in the database, then you should
 tell the user the record already exists and not add a new record.
 
-******(1 mark) Don't let the user enter invalid data. For instance, when
+(1 mark) Don't let the user enter invalid data. For instance, when
 entering the year a song was published, if the user enters out-of-range
 numbers, like -343 or 2201, then your program should re-ask the user for a
 valid number.
@@ -93,6 +92,7 @@ records where the typed-in string from the user exactly matches the
 appropriate field of a record.
 */
 void Database::stringSearch(std::string string){
+    bool flag = false;
     for(int i = 0; i< numSongs; i++){
         if(songs[i].getGenre() == string || 
            songs[i].getTitle() == string ||
@@ -102,10 +102,11 @@ void Database::stringSearch(std::string string){
                           << " Artist: " << songs[i].getArtist()
                           << " Year: " << songs[i].getYear() << std::endl;
             }
-        else {
-            std::cout << "No results found." << std::endl;
-        }
+            flag = true;
         
+    }
+    if (flag == false){
+        std::cout << "No results found." << std::endl;
     }
 }
 
@@ -113,17 +114,45 @@ void Database::stringSearch(std::string string){
 (1 mark) When searching for strings, one kind of search should print all
 records where the typed-in string from the user occurs as a substring in the
 appropriate field of a record.
+
+// find returns std::string::npos if no substring in fields matches the search string.
 */
 void Database::substringSearch(std::string string){
-    
+    bool flag = false;
+    for(int i = 0; i< numSongs; i++){
+        if(songs[i].getGenre().find(string) != std::string::npos || 
+           songs[i].getTitle().find(string) != std::string::npos ||
+           songs[i].getArtist().find(string) != std::string::npos){
+                std::cout << "Genre: " << songs[i].getGenre() 
+                          << " Title: " << songs[i].getTitle()
+                          << " Artist: " << songs[i].getArtist()
+                          << " Year: " << songs[i].getYear() << std::endl;
+            }
+        flag = true;
+    }
+    if (flag == false){
+        std::cout << "No results found." << std::endl;
+    }
 }
 
-
-void Database::numberSearch(int number){
+/*
+(1 mark) When searching for numbers, one kind of search should print all
+records where the typed-in number from the user exactly matches the
+appropriate field of a record.
+*/
+void Database::yearSearch(int number){
     return;
 }
 
-
+/*
+(1 mark) When searching for numbers, one kind of search should print all
+records where the number of the appropriate field of a record is in a range,
+from low to high. This would, for example, let the user see all songs in a
+song database from the year 2005 to 2015.
+*/
+void Database::rangeSearch(int number){
+    return;
+}
 
 
 // Deleting a Record 
